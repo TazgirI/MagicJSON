36963d3cdda829988c1da9c18459f6dbbe4ea8ef
TM316157 <tallenmcgill@gmail.com>
2025-12-26 02:46:50 +0000

    Added StatementOptional<T> for handling possible Statement values

---
+++ src/main/java/net/tazgirl/magicjson/StatementOptional.java
@@ -0,0 +1,36 @@
+package net.tazgirl.magicjson;
+
+import org.jetbrains.annotations.NotNull;
+
+public class StatementOptional<T>
+{
+    T value;
+    T defaultValue;
+
+    public StatementOptional(T value, @NotNull T defaultValue)
+    {
+        this.value = value;
+        this.defaultValue = defaultValue;
+    }
+
+    public T get()
+    {
+        if(value instanceof String string && PrivateCore.hasStatement(string))
+        {
+            Object statementResult = PrivateCore.runStatement(string);
+            try
+            {
+                return statementResult != null ? (T) statementResult : defaultValue;
+            }
+            catch (ClassCastException e)
+            {
+                return defaultValue;
+            }
+
+        }
+
+        return value != null ? value : defaultValue;
+    }
+
+
+}
