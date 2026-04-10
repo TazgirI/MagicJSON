95e72a2f9846558b54d4b34533b19ea22710e5f6
TM316157 <tallenmcgill@gmail.com>
2026-04-10 16:43:26 +0100

    Test changed itself again

--- src/main/java/net/tazgirl/magicjson/optionals/tests/ResultTest.java
+++ src/main/java/net/tazgirl/magicjson/optionals/tests/ResultTest.java
@@ -1,6 +1,14 @@
 package net.tazgirl.magicjson.optionals.tests;
 
-public record ResultTest<T>(Class<T> type)
+import com.mojang.datafixers.types.Func;
+import net.minecraft.core.Holder;
+import net.minecraft.world.effect.MobEffect;
+import org.jetbrains.annotations.NotNull;
+
+import javax.annotation.Nullable;
+import java.util.function.Function;
+
+public record ResultTest<T>(Class<T> type, Function<Object, Boolean> advancedTest)
 {
     public static final ResultTest<Integer> INTEGER = new ResultTest<>(Integer.class);
     public static final ResultTest<Double> DOUBLE = new ResultTest<>(Double.class);
@@ -8,10 +16,34 @@ public record ResultTest<T>(Class<T> type)
     public static final ResultTest<Long> LONG = new ResultTest<>(Long.class);
     public static final ResultTest<Boolean> BOOLEAN = new ResultTest<>(Boolean.class);
     public static final ResultTest<String> STRING = new ResultTest<>(String.class);
+    public static final ResultTest<Holder<MobEffect>> MOB_EFFECT = new ResultTest<>(object -> object instanceof Holder<?> holder && holder.value() instanceof MobEffect);
+
+    public ResultTest
+    {
+        if(type == null && advancedTest == null)
+        {
+            throw new IllegalArgumentException("ResultTest has a null type and a null advancedTest, at least one must not be null");
+        }
+    }
+
+    public ResultTest(@NotNull Function<Object, Boolean> advancedTest)
+    {
+        this(null, advancedTest);
+    }
+
+    public ResultTest(@NotNull Class<T> type)
+    {
+        this(type, null);
+    }
 
     public boolean test(Object input)
     {
-        return type.isInstance(input);
+        if(advancedTest == null)
+        {
+            return type.isInstance(input);
+        }
+
+        return advancedTest.apply(input);
     }
 
 }
