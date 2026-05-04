68cb6add7f9b56cf03d8f98e2150dda96bf9273f
Tazgirl <tallenmcgill@gmail.com>
2026-05-05 00:32:24 +0100

    Finished pointer fetch

--- src/main/java/net/tazgirl/magicjson/memory/Pointer.java
+++ src/main/java/net/tazgirl/magicjson/memory/Pointer.java
@@ -3,6 +3,8 @@ package net.tazgirl.magicjson.memory;
 import net.tazgirl.magicjson.statements.objects.StatementHolder;
 import net.tazgirl.magicjson.statements.objects.memory.MemoryObject;
 
+import java.util.Map;
+
 public class Pointer
 {
     final String name;
@@ -25,7 +27,16 @@ public class Pointer
             return holder.args.get(name);
         }
 
+        String address = name;
 
+        if(memoryNamespace == MemoryObject.Namespace.LOCAL)
+        {
+            address = holder.getNamespace() + ":" + address;
+        }
+
+        Map<String, Object> memoryMap = memorySpace == MemoryObject.Space.RUNTIME ? Memory.getRuntimeMemory() : Memory.getWorldMemoryMap();
+
+        return memoryMap.get(address);
     }
 
     public enum Location
